9cff6224f80f3e81cb246737a76d92ef4869aa58
Anastasia Minikes <minikes.a@mail.ru>
2023-05-08 23:42:58 +0700

    1000

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         int check = 300;
-        int replenishment = 1200;
+        int replenishment = 1000;
         int bonus = replenishment / 100;
 
         int finalCheck;
-        if (replenishment >= 1000) {
+        if (replenishment > 1000) {
             finalCheck = bonus + check + replenishment;
         } else {
             finalCheck = replenishment + check;
